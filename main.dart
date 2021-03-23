658ed533e487a588f5251c4697d56fadde319482
aditikagalkar <78659700+aditikagalkar@users.noreply.github.com>
2021-03-23 15:42:03 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -8,51 +8,144 @@ import 'package:flutter/services.dart';
 
 class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
+
 }
 
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.green[400]));
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 100,
-        backgroundColor: Colors.green,
-        centerTitle: true,
-        title: Text(
-          'Precious Plants',
-          style: TextStyle(color: Color(0xffffffff)),
+      body: Stack(
+        children: <Widget>[Container(
+          height: size.height * 0.45,
+          decoration: BoxDecoration(color: Color(0xFF4CAF50)),
         ),
-      ),
-      body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.all(Radius.circular(22.0))
-        ),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 5.0),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Search Phone",
-                    hintStyle: TextStyle(color: Colors.green[200]),
-                    icon: Icon(Icons.search, color: Colors.green[200]),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(12, 25, 30, 16),
+                  child: Text(
+                    "Precious Plants",
+                    style: Theme.of(context)
+                        .textTheme
+                        .display1
+                        .copyWith(
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 30),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                    decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(29.5),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "Search",
+                        border: InputBorder.none,
+                        icon: Icon(
+                          Icons.search,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child: GridView.count(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.85,
+                      mainAxisSpacing: 20,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: PlantCard(
+                            title: "Plant 1"
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: PlantCard(
+                              title: "Plant 2"
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: PlantCard(
+                              title: "Plant 3"
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: PlantCard(
+                              title: "Plant 4"
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: PlantCard(
+                              title: "Plant 5"
+                          ),
+                        ),
+                      ],
+                    )
+                ),
+              ],
+          ),
             ),
-
-          ],
-        ),
+          )
+        ],
       ),
     );
+  }
+
+}
+
+class PlantCard extends StatelessWidget {
+  final String svgSrc;
+  final String title;
+  const PlantCard({
+    Key key, this.svgSrc, this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+       decoration: BoxDecoration(
+         color: Colors.white,
+         borderRadius: BorderRadius.circular(13),
+         boxShadow: [BoxShadow(
+           offset: Offset(0, 17),
+           blurRadius: 17,
+           spreadRadius: -23,
+           color: Colors.grey[500],
+         ),
+         ],
+       ),
+       child: Column(
+         children: <Widget>[
+           Spacer(),
+           //SvgPicture.asset(svgSrc),
+           Text(
+             title,
+             textAlign: TextAlign.center,
+             style: Theme.of(context)
+               .textTheme
+               .title
+               .copyWith(fontSize: 15)
+           ),
+         ],
+       ),
+     );
   }
 }
 
